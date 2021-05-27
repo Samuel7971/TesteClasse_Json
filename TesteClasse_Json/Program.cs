a4f694fc0ebd4bf2d1ff137c9397f7778d7eb5f2
Samuel Rodrigues <samuelrodr.sr@gmail.com>
2021-05-27 12:22:50 -0300

    Add project files.

---
+++ TesteClasse_Json/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace TesteClasse_Json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Questão 4
+            var clsTeste = new ClsTeste()
+            {
+                Codigo = 1000,
+                Descricao = "teste"
+            };
+
+            //Questão 5
+            string output = JsonConvert.SerializeObject(clsTeste);
+            Console.WriteLine(output);
+
+            Console.ReadKey();            
+        }
+    }
+}
